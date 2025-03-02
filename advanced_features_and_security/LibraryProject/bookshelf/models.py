3fb236682ad2faa12e6e969d44935f87d1a56ec3
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-02 21:57:05 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,29 @@
 from django.db import models  
-from django.contrib.auth.models import AbstractUser  
+from django.contrib.auth.models import AbstractUser, BaseUserManager  
 
+# Custom User Manager  
+class CustomUserManager(BaseUserManager):  
+    def create_user(self, email, password=None, **extra_fields):  
+        """Create and return a regular user with an email and password."""  
+        if not email:  
+            raise ValueError('The Email field must be set')  
+        email = self.normalize_email(email)  
+        user = self.model(email=email, **extra_fields)  
+        user.set_password(password)  
+        user.save(using=self._db)  
+        return user  
+
+    def create_superuser(self, email, password=None, **extra_fields):  
+        """Create and return a superuser with an email and password."""  
+        extra_fields.setdefault('is_staff', True)  
+        extra_fields.setdefault('is_superuser', True)  
+
+        return self.create_user(email, password, **extra_fields)  
+
+# Custom User Model  
 class CustomUser(AbstractUser):  
     date_of_birth = models.DateField(null=True, blank=True)  
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)  
+
+    # Use the custom user manager  
+    objects = CustomUserManager()
