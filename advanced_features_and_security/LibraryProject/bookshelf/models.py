51850cd8c1db8b5aefadfbb5314718e8f4ceb976
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-02 21:55:51 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,9 +1,6 @@
 from django.db import models  
+from django.contrib.auth.models import AbstractUser  
 
-class Book(models.Model):  
-    title = models.CharField(max_length=200)  
-    author = models.CharField(max_length=100)  
-    publication_year = models.IntegerField()  
-
-    def __str__(self):  
-        return self.title
+class CustomUser(AbstractUser):  
+    date_of_birth = models.DateField(null=True, blank=True)  
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
