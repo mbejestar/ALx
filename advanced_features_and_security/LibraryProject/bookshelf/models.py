69a3cc94ebb7a2f794a90baf9d35cfdfcc812f85
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-02 22:19:31 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -27,3 +27,16 @@ class CustomUser(AbstractUser):
 
     # Use the custom user manager  
     objects = CustomUserManager()
+
+###
+class Article(models.Model):  
+    title = models.CharField(max_length=200)  
+    content = models.TextField()  
+    
+    class Meta:  
+        permissions = [  
+            ("can_view", "Can view article"),  
+            ("can_create", "Can create article"),  
+            ("can_edit", "Can edit article"),  
+            ("can_delete", "Can delete article"),  
+        ]  
