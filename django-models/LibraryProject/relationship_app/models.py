c009d06ce195e3554a61dc3c00df45d2cc04c4f1
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-02 20:36:58 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,28 +1,92 @@
-from django.db import models  
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class Author(models.Model):  
-    name = models.CharField(max_length=100)  
+# Create your models here.
 
-    def __str__(self):  
-        return self.name  
+# Define roles
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+]
 
-class Book(models.Model):  
-    title = models.CharField(max_length=200)  
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
-    def __str__(self):  
-        return self.title  
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
 
-class Library(models.Model):  
-    name = models.CharField(max_length=100)  
-    books = models.ManyToManyField(Book, related_name='libraries')  
+# Automatically create UserProfile when a User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
 
-    def __str__(self):  
-        return self.name  
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
-class Librarian(models.Model):  
-    name = models.CharField(max_length=100)  
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')  
-
-    def __str__(self):  
+# 1. ForeignKey Relationship
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    def __str__(self):
+        return self.title
+
+# 2. ManyToMany Relationship
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
+    
+    def __str__(self):
+        return self.name
+
+# 3. OneToOne Relationship
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(
+        Library,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='librarian'
+    )
+    
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        permissions = [
+            ("can_add_library", "Can add library"),
+            ("can_change_library", "Can change library"),
+            ("can_delete_library", "Can delete library"),
+        ]
