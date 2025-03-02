9db93c50da319b5142a666155291e9e38cd2082f
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-02 22:39:52 +0200

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,13 @@
-<form method="post">  
-    {% csrf_token %}  
-    <!-- form fields go here -->  
-</form>
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Book, CustomUser
+
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Name', widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}))
+    bio = forms.CharField(label='Bio', widget=forms.Textarea(attrs={'placeholder': 'Tell us about yourself'}), required=False)
+
+class CustomUserCreationForm(UserCreationForm):
+    ROLES = [
+
+    ]
