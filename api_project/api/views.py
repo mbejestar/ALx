3d146383436524015210bb3cd9a7048ff46a2b32
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-06 22:05:12 +0200

    Delete api_project directory

--- api_project/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics  
-from .models import Book  
-from .serializers import BookSerializer  
-
-class BookList(generics.ListAPIView):  
-    queryset = Book.objects.all()  # Retrieve all Book objects  
-    serializer_class = BookSerializer  # Use the BookSerializer to serialize the data  
