c8cfdef800b96b473c36c183bfc43c0402da0890
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-24 06:40:29 +0200

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path  
 from .views import UserRegistrationView, CustomAuthToken  
+
+urlpatterns = [  
+    path('register/', UserRegistrationView.as_view(), name='user-register'),  
+    path('login/', CustomAuthToken.as_view(), name='user-login'),  
+]  
