f5db8ac4fc5e07ddce3c3f38563e4e1d14c25878
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-24 06:39:06 +0200

    updating serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,14 +1,40 @@
-from rest_framework import serializers  
-from .models import CustomUser  
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-class CustomUserSerializer(serializers.ModelSerializer):  
-    class Meta:  
-        model = CustomUser  
-        fields = ['id', 'username', 'password', 'bio', 'profile_picture']  
-        extra_kwargs = {'password': {'write_only': True}}  
+User = get_user_model()
 
-    def create(self, validated_data):  
-        user = CustomUser(**validated_data)  
-        user.set_password(validated_data['password'])  
-        user.save()  
-        return user  
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', None),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        Token.objects.create(user=user)
+        return user
+
+
+class TokenSerializer(serializers.Serializer):
+    token = serializers.CharField()
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'bio', 'profile_picture', 'followers']
+        read_only_fields = ['email', 'followers']
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email']
