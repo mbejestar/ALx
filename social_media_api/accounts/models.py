3e72abbbc152ad325264a82c93ef15ba936e4b56
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-25 20:55:01 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -13,3 +13,21 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+#views in the accounts app that allow users to follow and unfollow others. 
+
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+CustomUser  = get_user_model()
+
+class UserListView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        users = self.get_queryset()
+        user_data = [{'id': user.id, 'username': user.username} for user in users]
+        return Response(user_data)
