36164bf1938a97a1565d4b9e0d3941e6483dfe95
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-25 20:51:24 +0200

    Social media api alx

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,10 +1,15 @@
-from django.contrib.auth.models import AbstractUser  
-from django.db import models  
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-class CustomUser(AbstractUser):  
-    bio = models.TextField(blank=True)  
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)  
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='followed_by', blank=True)  
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)  # Users biography
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)  # Profile pictureupload
+    following = models.ManyToManyField(
+        'self',  # Self-referential relationship
+        symmetrical=False,  
+        related_name='followers',  # Reverse relationship for followers
+        blank=True
+    )
 
-    def __str__(self):  
-        return self.username  
+    def __str__(self):
+        return self.username
