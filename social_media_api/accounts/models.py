5b8a990e8f2e0750c6fcf6fba1a817a3a77a6cde
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-25 20:55:59 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,18 +1,30 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
-class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True, null=True)  # Users biography
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)  # Profile pictureupload
-    following = models.ManyToManyField(
-        'self',  # Self-referential relationship
-        symmetrical=False,  
-        related_name='followers',  # Reverse relationship for followers
-        blank=True
-    )
+User = get_user_model()
 
-    def __str__(self):
-        return self.username
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'message': f'You are now following {user_to_follow.username}'})
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = User.objects.get(id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'message': f'You have unfollowed {user_to_unfollow.username}'})
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+    
 
 #views in the accounts app that allow users to follow and unfollow others. 
 
