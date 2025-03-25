a32d753354ef1fdd6540769630b638a0a9f863e6
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-25 22:24:56 +0200

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import follow_user, unfollow_user, UserListView
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
-    path('follow/<int:user_id>/', follow_user, name='follow-user'),
-    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
-    path('users/', UserListView.as_view(), name='user-list'),
+    path('api_token/', obtain_auth_token, name='api_token'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
 ]
