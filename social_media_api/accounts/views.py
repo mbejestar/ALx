9c52846877655776f7a496eb446ed5a6867b3a32
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-25 21:04:44 +0200

    Social media api alx

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,23 +1,69 @@
-from rest_framework import generics, permissions  
-from rest_framework.authtoken.models import Token  
-from rest_framework.response import Response  
-from rest_framework.authtoken.views import ObtainAuthToken  
-from .models import CustomUser  
-from .serializers import CustomUserSerializer  
+```python
+from rest_framework import generics, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
-# User Registration View  
-class UserRegistrationView(generics.CreateAPIView):  
-    queryset = CustomUser.objects.all()  
-    serializer_class = CustomUserSerializer  
-    permission_classes = [permissions.AllowAny]  
+# User Registration View
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [permissions.AllowAny]
 
-    def create(self, request, *args, **kwargs):  
-        response = super().create(request, *args, **kwargs)  
-        return Response({"token": Token.objects.create(user=self.get_object()).key})  
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response({"token": Token.objects.create(user=self.get_object()).key})
 
-# Custom Auth Token View  
-class CustomAuthToken(ObtainAuthToken):  
-    def post(self, request, *args, **kwargs):  
-        response = super().post(request, *args, **kwargs)  
-        token = Token.objects.get(user=response.data['user'])  
-        return Response({'token': token.key})  
+# Custom Auth Token View
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(user=response.data['user'])
+        return Response({'token': token.key})
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'message': f'You are now following {user_to_follow.username}'})
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = User.objects.get(id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'message': f'You have unfollowed {user_to_unfollow.username}'})
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+
+#phase2 
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
+
+class UserListView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        users = self.get_queryset()
+        user_data = [{'id': user.id, 'username': user.username} for user in users]
+        return Response(user_data)
+```
