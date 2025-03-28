23f7c426c2ed2062e9e937013009795963b05475
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-03-28 23:01:52 +0200

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r%(kxpamp_--s0!616#xm5lfeu*x3i7j=^on+2@bw8dn3&c18q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
